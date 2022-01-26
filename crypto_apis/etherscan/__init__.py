d08807aa11d2f86fb42c29f6def801acfca85854
Unique-Divine <realuniquedivine@gmail.com>
2022-01-26 10:55:12 -0600

    feature (__init__.py): Allow etherscan types to be imported with .

--- crypto_apis/etherscan/__init__.py
+++ crypto_apis/etherscan/__init__.py
@@ -1,4 +1,5 @@
 """TODO module docs for crypto_apis.etherscan"""
+from crypto_apis.etherscan import types 
 from crypto_apis.etherscan import etherscan_connector
 from crypto_apis.etherscan import token_info_connector 
 
@@ -6,5 +7,9 @@ EtherscanConnector = etherscan_connector.EtherscanConnector
 TokenInfoConnector = token_info_connector.TokenInfoConnector
 # TokenInfoConnector.__doc__ = 
 """TODO doc"""
+
+InternalMsgCall = types.InternalMsgCall
+NormalTx = types.NormalTx
+TxReceipt = types.TxReceipt
 
 __all__ = ['EtherscanConnector', 'TokenInfoConnector']
