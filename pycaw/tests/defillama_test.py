6db5b96e2fd039e3ea2916edcd1cbdd49da1c203
Unique-Divine <realuniquedivine@gmail.com>
2022-03-17 09:52:42 -0400

    tests (defillama): All passing except get_protocols

--- pycaw/tests/defillama_test.py
+++ pycaw/tests/defillama_test.py
@@ -1,54 +1,49 @@
-import unittest
-from messari.defillama import DeFiLlama
 import pandas as pd
+from pycaw import defillama as dl 
 
 
-class TestDeFiLlama(unittest.TestCase):
-    """This is a unit testing class for testing the DeFiLlama class"""
+class TestDeFiLlama():
+    """Test suite for the 'defillama.DeFiLlama' class."""
 
     def test_init(self):
         """Test initializing DeFiLlama class"""
-        dl = DeFiLlama()
-        self.assertIsInstance(dl, DeFiLlama)
+        dl_conn = dl.DeFiLlama()
+        assert isinstance(dl_conn, dl.DeFiLlama)
 
     def test_get_protocol_tvl(self):
         """Test getting protocol tvl"""
-        dl = DeFiLlama()
-        tvl = dl.get_protocol_tvl_timeseries(
+        dl_conn = dl.DeFiLlama()
+        tvl = dl_conn.get_protocol_tvl_timeseries(
             ["aave", "compound"], start_date="2021-10-01", end_date="2021-10-10"
         )
-        self.assertIsInstance(tvl, pd.DataFrame)
+        assert isinstance(tvl, pd.DataFrame)
 
     def test_global_tvl(self):
         """Test getting global tvl"""
-        dl = DeFiLlama()
-        global_tvl = dl.get_global_tvl_timeseries(
+        dl_conn = dl.DeFiLlama()
+        global_tvl = dl_conn.get_global_tvl_timeseries(
             start_date="2021-10-01", end_date="2021-10-10"
         )
-        self.assertIsInstance(global_tvl, pd.DataFrame)
+        assert isinstance(global_tvl, pd.DataFrame)
 
     def test_chain_tvl(self):
         """Test getting chain tvl"""
-        dl = DeFiLlama()
+        dl_conn = dl.DeFiLlama()
         chains = ["Avalanche", "Harmony", "Polygon"]
-        chain_tvl = dl.get_chain_tvl_timeseries(
+        chain_tvl = dl_conn.get_chain_tvl_timeseries(
             chains, start_date="2021-10-01", end_date="2021-10-10"
         )
-        self.assertIsInstance(chain_tvl, pd.DataFrame)
+        assert isinstance(chain_tvl, pd.DataFrame)
 
     def test_current_tvl(self):
         """Test getting current protocol tvl"""
-        dl = DeFiLlama()
+        dl_conn = dl.DeFiLlama()
         protocols = ["uniswap", "curve", "aave"]
-        current_tvl = dl.get_current_tvl(protocols)
-        self.assertIsInstance(current_tvl, pd.DataFrame)
+        current_tvl = dl_conn.get_current_tvl(protocols)
+        assert isinstance(current_tvl, pd.DataFrame)
 
     def test_get_protocols(self):
         """Test getting protocol info"""
-        dl = DeFiLlama()
-        protocols = dl.get_protocols()
-        self.assertIsInstance(protocols, pd.DataFrame)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        dl_conn = dl.DeFiLlama()
+        protocols = dl_conn.get_protocols()
+        assert isinstance(protocols, pd.DataFrame)
